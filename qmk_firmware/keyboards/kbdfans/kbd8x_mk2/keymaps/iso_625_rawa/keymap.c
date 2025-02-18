5aacbd4662b00bac73ae672bc67346b556c36b8d
David Göransson <david.goransson@mullvad.net>
2025-02-18 08:08:43 +0100

    Update mkii layout

--- qmk_firmware/keyboards/kbdfans/kbd8x_mk2/keymaps/iso_625_rawa/keymap.c
+++ qmk_firmware/keyboards/kbdfans/kbd8x_mk2/keymaps/iso_625_rawa/keymap.c
@@ -16,27 +16,17 @@
 #include QMK_KEYBOARD_H
 
 #define BL 0     // Base Layer
-#define EL 1     // Function Layer
-#define NL 2     // Easy Access Layer
+#define NL 1     // Easy Access Layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BL] = LAYOUT_tkl_iso( /* Base Layer */
 
-  	KC_ESC,                       KC_F1,        KC_F2,        KC_F3,        KC_F4,             KC_F5,        KC_F6,        KC_F7,        KC_F8,              KC_F9,        KC_F10,        KC_F11,        KC_F12,          KC_PSCR, KC_SLCK, KC_PAUS, \
+  	KC_ESC,                       KC_F1,        KC_F2,        KC_F3,        KC_F4,             KC_F5,        KC_F6,        KC_F7,        KC_F8,              KC_F9,        KC_F10,        KC_F11,        KC_F12,          KC_PSCR, KC_SLCT, KC_PAUS, \
   	KC_GRV,         KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,      KC_EQL,                   KC_BSPC,          KC_INS,  KC_HOME, KC_PGUP, \
   	LT(NL, KC_TAB), KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_LBRC,      KC_RBRC,                                    KC_DEL,  KC_END,  KC_PGDN, \
-  	LT(EL, KC_ESC), KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      KC_QUOT,      KC_NUHS,                   KC_ENT,                                     \
+  	KC_CAPS,        KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      KC_QUOT,      KC_NUHS,                   KC_ENT,                                     \
   	KC_LSFT,        KC_NUBS,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                                KC_RSFT,                   KC_UP,            \
   	KC_LCTL,        KC_LGUI,      KC_LALT,                                               KC_SPC,                                                KC_ALGR,           KC_RGUI,          KC_APP,            KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
-  ),
-
-  [EL] = LAYOUT_tkl_iso( /* Easy Access Layer */
-  	_______,                      _______,      _______,      _______,      _______,           _______,      _______,      _______,      _______,            _______,      _______,       _______,     _______,           _______, _______, _______, \
-  	_______,        _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,                 _______,           _______, _______, _______, \
-  	_______,        _______,      KC_PGUP,      _______,      _______,      _______,      _______,      RALT(KC_7),   RALT(KC_0),   _______,      _______,      _______,      _______,                                    _______, _______, _______, \
-  	_______,        KC_HOME,      KC_PGDN,      KC_END,      _______,      _______,      LSFT(KC_7),   LSFT(KC_8),   LSFT(KC_9),   RALT(KC_MINS),_______,      _______,      _______,                 _______,                                      \
-  	_______,        _______,      _______,      _______,      _______,      _______,      _______,      KC_NUBS,      RALT(KC_8),   RALT(KC_9),   LSFT(KC_NUBS),_______,                               _______,                    _______,          \
-  	_______,        _______,      _______,                                               _______,                                               _______,           _______,          _______,          _______,           _______, _______, _______  \
   ),
 
   [NL] = LAYOUT_tkl_iso( /* Numpad layer */
